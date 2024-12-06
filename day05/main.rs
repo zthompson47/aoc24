e10a129619474d21051d99efc15dd751e862a312
Zach Thompson <zach@allotropic.com>
2024-12-05 19:43:30 -0500

    Solve day 5

--- day05/main.rs
+++ day05/main.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 
 fn main() {
-    println!("Part 1: {}", part1());
+    let (part1, bad_manuals, no_follow) = part1();
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2(bad_manuals, no_follow));
 }
 
-fn part1() -> u32 {
+fn part1() -> (u32, Vec<Vec<u32>>, HashMap<u32, Vec<u32>>) {
     let mut no_follow: HashMap<u32, Vec<u32>> = HashMap::new();
     let mut manuals: Vec<Vec<u32>> = Vec::new();
+    let mut bad_manuals: Vec<Vec<u32>> = Vec::new();
     let mut reached_manuals = false;
     let mut result = 0;
 
@@ -25,17 +28,12 @@ fn part1() -> u32 {
         }
     });
 
-    //println!("{no_follow:#?}");
-    //println!("{manuals:#?}");
-
     'manual: for manual in manuals {
         let mut illegal: Vec<u32> = Vec::new();
-        //println!("Trying: {manual:?}");
 
         for page in &manual {
-            //println!("{illegal:?} {page}");
             if illegal.contains(page) {
-                //println!("continuing...");
+                bad_manuals.push(manual);
                 continue 'manual;
             }
             if let Some(pages) = no_follow.get(page) {
@@ -43,13 +41,33 @@ fn part1() -> u32 {
             }
         }
 
-        //println!("illegal: {illegal:?}");
-        //println!("good: {manual:?}");
-        //println!();
-
         let middle = manual[manual.len() / 2];
         result += middle;
     }
+
+    (result, bad_manuals, no_follow)
+}
+
+fn part2(bad_manuals: Vec<Vec<u32>>, no_follow: HashMap<u32, Vec<u32>>) -> u32 {
+    let mut result = 0;
+
+    bad_manuals.iter().for_each(|manual| {
+        let mut fixed: Vec<u32> = Vec::new();
+
+        for page in manual {
+            let mut insert_at = fixed.len();
+            for i in (0..fixed.len()).rev() {
+                if let Some(illegal) = no_follow.get(&fixed[i]) {
+                    if illegal.contains(page) {
+                        insert_at = i;
+                    }
+                }
+            }
+            fixed.insert(insert_at, *page);
+        }
+
+        result += fixed[fixed.len() / 2];
+    });
 
     result
 }
