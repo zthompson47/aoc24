0391fbd9853557dbb344840049c5a1676e513925
Zach Thompson <zach@allotropic.com>
2024-12-11 11:43:59 -0500

    Solve day 10 part 2

--- day10/main.rs
+++ day10/main.rs
@@ -1,10 +1,12 @@
 use std::collections::HashSet;
 
 fn main() {
-    println!("Part 1: {}", part1());
+    let (part1, part2) = run();
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn part1() -> usize {
+fn run() -> (usize, usize) {
     let trail_map: Vec<Vec<u32>> =
         include_str!("input.txt")
             .lines()
@@ -13,22 +15,36 @@ fn part1() -> usize {
                 acc
             });
     let mut total_score = 0;
+    let mut total_rating = 0;
     for row in 0..trail_map.len() {
         for column in 0..trail_map[0].len() {
             if trail_map[row][column] == 0 {
-                let score = trailhead_peaks(&trail_map, Position { row, column });
-                total_score += score.len();
+                total_score += trailhead_peaks(&trail_map, Position { row, column }).len();
+                total_rating += trailhead_rating(&trail_map, Position { row, column });
             }
         }
     }
-    total_score
+    (total_score, total_rating)
 }
 
-fn trailhead_peaks(
-    trail_map: &[Vec<u32>],
-    position: Position,
-    //_cache: &mut HashMap<Position, HashSet<Position>>,
-) -> HashSet<Position> {
+fn trailhead_rating(trail_map: &[Vec<u32>], position: Position) -> usize {
+    adjacent(trail_map, position)
+        .iter()
+        .map(|(adjacent_position, height)| {
+            if *height == trail_map[position.row][position.column] + 1 {
+                if *height == 9 {
+                    1
+                } else {
+                    trailhead_rating(trail_map, *adjacent_position)
+                }
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn trailhead_peaks(trail_map: &[Vec<u32>], position: Position) -> HashSet<Position> {
     let peaks: Vec<HashSet<Position>> = adjacent(trail_map, position)
         .iter()
         .map(|(adjacent_position, height)| {
