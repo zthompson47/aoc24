9c1120040d0f9e087d7e9efdea9dbfc2c5275b60
Zach Thompson <zach@allotropic.com>
2024-12-16 06:10:53 -0500

    Solve day 13 part 2

--- day13/main.rs
+++ day13/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Part 1: {}", part1());
+    //println!("Part 1: {}", part1());
     println!();
     println!("Part 2: {}", part2());
 }
@@ -9,6 +9,7 @@ fn part1() -> usize {
     let mut cost = 0;
 
     for machine in machines {
+        println!("{machine:?}");
         let mut costs: Vec<usize> = Vec::new();
 
         for a in 0..=100 {
@@ -16,7 +17,7 @@ fn part1() -> usize {
                 let x = machine.a.0 * a + machine.b.0 * b;
                 let y = machine.a.1 * a + machine.b.1 * b;
                 if x == machine.prize.0 && y == machine.prize.1 {
-                    println!("{machine:?}");
+                    //println!("{machine:?}");
                     println!("a={a} b={b} cost={}", a * 3 + b);
                     costs.push(a * 3 + b);
                 }
@@ -41,8 +42,8 @@ fn part2() -> usize {
     let mut cost = 0;
 
     for machine in machines.iter_mut() {
-        //machine.prize.0 += 10000000000000;
-        //machine.prize.1 += 10000000000000;
+        machine.prize.0 += 10000000000000;
+        machine.prize.1 += 10000000000000;
         println!("_____________________________");
         println!("{machine:?}");
         let Machine { a, b, prize } = machine;
@@ -51,23 +52,26 @@ fn part2() -> usize {
         let max_a_y = prize.1 / a.1;
         let max_b_x = prize.0 / b.0;
         let max_b_y = prize.1 / b.1;
-        println!("max_a_x={max_a_x} max_a_y={max_a_y} max_b_x={max_b_x} max_b_y={max_b_y}");
+        //println!("max_a_x={max_a_x} max_a_y={max_a_y} max_b_x={max_b_x} max_b_y={max_b_y}");
 
         let mut x_fits = Vec::new();
         for i in (0..=max_b_x).rev() {
             let delta = prize.0 - b.0 * i;
-            //println!("try delta x {delta}, i={i}");
+            //println!("try delta x {delta}, i={i}, {}%", delta as f64 * 100.0 / prize.0 as f64);
             if delta > a.0 * b.0 * 2 {
-                //break;
+                break;
             }
             if delta % a.0 == 0 {
                 let distance = delta / a.0;
-                println!("GOT ONE X - b.x={i} a.x={distance}");
+                //println!("GOT ONE X - b.x={i} a.x={distance}");
                 x_fits.push((i, distance));
+            }
+            if x_fits.len() > 1 {
+                break;
             }
         }
 
-        println!("----");
+        //println!("----");
 
         let mut y_fits = Vec::new();
         for i in (0..=max_b_y).rev() {
@@ -78,13 +82,79 @@ fn part2() -> usize {
             }
             if delta % a.1 == 0 {
                 let distance = delta / a.1;
-                println!("GOT ONE Y - b.y={i} a.y={distance}");
+                //println!("GOT ONE Y - b.y={i} a.y={distance}");
                 y_fits.push((i, distance));
+            }
+            if y_fits.len() > 1 {
+                break;
             }
         }
 
-        println!("___FITS: {x_fits:?}, {y_fits:?}");
+        println!("___FITS: x_len: {}, y_len: {}", x_fits.len(), y_fits.len());
+        println!("___FITS: x: {:?}, y: {:?}", x_fits, y_fits);
 
+        if x_fits.len() == 2 && y_fits.len() == 2 {
+            let x_fits = x_fits
+                .iter()
+                .map(|(x, y)| (*x as i64, *y as i64))
+                .collect::<Vec<_>>();
+            let y_fits = y_fits
+                .iter()
+                .map(|(x, y)| (*x as i64, *y as i64))
+                .collect::<Vec<_>>();
+            if let (&[(x1, y1), (x2, y2)], &[(x3, y3), (x4, y4)]) = (&*x_fits, &*y_fits) {
+                println!("...... {x1} {x2} {x3} {x4} {y1} {y2} {y3} {y4}");
+                let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+                if denominator == 0 {
+                    println!("...... Parallel or coincident!");
+                } else {
+                    let x_numerator =
+                        (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
+                    let y_numerator =
+                        (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+                    let x = x_numerator / denominator;
+                    let y = y_numerator / denominator;
+                    if x >= 0 && y >= 0 {
+                        let new_cost = x as usize + 3 * y as usize;
+                        println!("------ Intersects! x={x} y={y} cost={new_cost}");
+                        if x as usize * b.0 + y as usize * a.0 == prize.0
+                            && x as usize * b.1 + y as usize * a.1 == prize.1
+                        {
+                            println!("------ and in rnage! cost={new_cost}");
+                            cost += new_cost;
+                        }
+                    } else {
+                        println!("------ negative IMPOSSIBLE");
+                    }
+                }
+            }
+        } else if x_fits.is_empty() || y_fits.is_empty() {
+            println!("------ zero IMPOSSIBLE");
+            continue;
+        } else if x_fits.len() == 1 && y_fits.len() == 1 && x_fits[0] == y_fits[0] {
+            let (x, y) = y_fits[0];
+            let new_cost = x + 3 * y;
+            println!("------ single match x={x} y={y} cost={new_cost}");
+            cost += new_cost;
+        } else if x_fits.len() == 1 && y_fits.len() == 2 {
+            println!("-------------------------x1");
+            let (x, y) = x_fits[0];
+            if x * b.0 + y * a.0 == prize.0 && x * b.1 + y * a.1 == prize.1 {
+                let new_cost = x + 3 * y;
+                println!("---- x1 YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY cost={new_cost}");
+                cost += new_cost;
+            }
+        } else if x_fits.len() == 2 && y_fits.len() == 1 {
+            println!("-------------------------y1");
+            let (x, y) = y_fits[0];
+            if x * b.0 + y * a.0 == prize.0 && x * b.1 + y * a.1 == prize.1 {
+                let new_cost = x + 3 * y;
+                println!("---- y1 YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY cost={new_cost}");
+                cost += new_cost;
+            }
+        }
+
+        /*
         let mut step = None;
         if x_fits.len() > 0 && y_fits.len() > 0 {
             let x_step = if x_fits.len() == 1 {
@@ -133,6 +203,7 @@ fn part2() -> usize {
                 }
             }
         }
+        */
 
         //if i == 0 {
         //    break;
