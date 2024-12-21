5a83d861f4e8505c0c371e42d5d9a5bd47514885
Zach Thompson <zach@allotropic.com>
2024-12-21 13:57:05 -0500

    Solve day 19 part 2

--- day19/main.rs
+++ day19/main.rs
@@ -6,7 +6,6 @@ fn main() {
     let designs: Vec<&str> = lines.skip(1).collect();
 
     patterns.sort();
-    //println!("Patterns: {patterns:#?}");
 
     let part1: usize = designs
         .iter()
@@ -14,11 +13,114 @@ fn main() {
         .count();
 
     println!("Part 1: {part1}");
+
+    let part2: usize = designs
+        .iter()
+        .map(|design| {
+            println!("_________________\ntrying {design}");
+            let matches = matches(design, &patterns);
+            //println!("matches: {matches:?}");
+            let mut visited = HashMap::new();
+            let solution = solutions(design.len(), &matches, 0, &mut visited);
+            //println!("got {solution}");
+            solution
+        })
+        .sum();
+
+    println!("Part 2: {part2}");
 }
 
-fn is_possible(design: &str, patterns: &[&str]) -> bool {
-    println!("design: {design}");
+fn solutions(
+    remaining: usize,
+    matches: &[HashMap<usize, Vec<&str>>],
+    level: usize,
+    visited: &mut HashMap<usize, usize>,
+) -> usize {
+    let spaces = format!("{:1$}", " ", level * 2);
+    //println!("{spaces}______level:{level}");
+    if level > matches.len() {
+        return 0;
+    }
 
+    if visited.contains_key(&level) {
+        return *visited.get(&level).unwrap();
+    }
+
+    // Start with longest substring matches.
+    let mut keys = matches[level].keys().collect::<Vec<_>>();
+    keys.sort_by(|a, b| b.cmp(a));
+
+    let mut result = 0;
+    for len in keys {
+        //println!("{spaces}len:{len}");
+        let match_count = matches[level].get(len).unwrap().len();
+        //println!("{spaces}match_count:{match_count}");
+
+        #[allow(clippy::comparison_chain)]
+        if *len == remaining {
+            //println!("{spaces}MATCH REMAINING: add {match_count}");
+            result += match_count;
+        } else if *len < remaining {
+            let to_add = match_count * solutions(remaining - *len, matches, level + *len, visited);
+            //println!("{spaces}NOT MATCH REMAINING: add {to_add}");
+            result += to_add;
+        }
+    }
+    visited.insert(level, result);
+
+    //println!("{spaces}**return** result:{result}");
+    result
+}
+
+/*
+fn solutions(
+    design_len: usize,
+    matches: &[HashMap<usize, Vec<&str>>],
+    level: usize,
+    //visited: &mut Vec<usize>,
+    visited: &mut HashMap<usize, usize>,
+) -> usize {
+    println!("\nlevel:{level}");
+    if level > matches.len() {
+        return 0;
+    }
+    if visited.contains_key(&level) {
+        return *visited.get(&level).unwrap();
+    }
+
+    let mut keys = matches[level].keys().collect::<Vec<_>>();
+    keys.sort_by(|a, b| b.cmp(a));
+
+    let mut result = 0;
+    for len in keys {
+        println!("  try len:{len}");
+        let match_count = matches.get(*len).unwrap().len();
+
+        if design_len == *len {
+            result += match_count;
+            println!("     end match, adding: {match_count}");
+        }
+        if design_len > *len {
+            let remaining = solutions(design_len - len, matches, level + len, visited);
+            result += match_count * remaining;
+            println!("     remaingin match, adding: {} * {} ", match_count, remaining );
+        }
+    }
+    visited.insert(level, result);
+
+    //println!("returning: {result}");
+
+    result
+}
+*/
+
+fn is_possible(design: &str, patterns: &[&str]) -> bool {
+    let matches = matches(design, patterns);
+    let mut visited = Vec::new();
+    matches_fit(design.len(), &matches, 0, &mut visited)
+}
+
+fn matches<'a>(design: &str, patterns: &'a [&str]) -> Vec<HashMap<usize, Vec<&'a str>>> {
     // Count possible matches by pattern length at each color in design.
     let mut matches: Vec<HashMap<usize, Vec<&str>>> = Vec::new();
     for i in 0..design.len() {
@@ -33,11 +135,7 @@ fn is_possible(design: &str, patterns: &[&str]) -> bool {
         }
         matches.push(sizes);
     }
-    //println!("    matches {:?}", matches);
-
-    //tail_matches(design, &matches)
-    let mut visited = Vec::new();
-    matches_fit(design.len(), &matches, 0, &mut visited)
+    matches
 }
 
 fn matches_fit(
@@ -46,73 +144,22 @@ fn matches_fit(
     level: usize,
     visited: &mut Vec<usize>,
 ) -> bool {
-    //println!("___________________");
-    if visited.contains(&level) {
+    if level > matches.len() {
         return false;
     }
-    if level < 20 {
-        println!(
-            "level: {level} design_len: {design_len} {:?}",
-            matches[level]
-        );
-    }
-    if level > matches.len() {
-        //println!("REACED END");
+    if visited.contains(&level) {
         return false;
     }
     let mut keys = matches[level].keys().collect::<Vec<_>>();
     keys.sort_by(|a, b| b.cmp(a));
     for len in keys {
-        //println!("try len:{len}");
         if design_len == *len {
-            println!("MATCHED: {len}");
             return true;
         }
         if design_len > *len && matches_fit(design_len - len, matches, level + len, visited) {
-            println!("NEXT LEVEL: {}", level + len);
             return true;
         }
     }
-    //println!("FALSE");
     visited.push(level);
     false
 }
-
-/*
-fn tail_matches(design: &str, matches: &[HashMap<usize, Vec<&str>>]) -> bool {
-    println!("  tail_matches: {design}");
-    //println!("    matches {:?}", matches);
-    println!("    index: {}", design.len() - 1);
-    println!("    should try {:?}", matches[design.len() - 1].keys());
-    for len in matches[design.len() - 1].keys() {
-        println!("   trying len:{len}");
-        if *len == design.len() - 1 || tail_matches(&design[0..design.len() - len], matches) {
-            println!("----------TRUE----------------");
-            return true;
-        }
-    }
-    false
-}
-*/
-
-/*
-fn is_possible(design: &str, patterns: &[&str]) -> bool {
-    for pattern in patterns {
-        if design == *pattern {
-            return true;
-        }
-    }
-    for pattern in patterns {
-        //println!("trying pattern {pattern}");
-        if let Some(design) = design.strip_prefix(*pattern) {
-            //println!("found prefix {pattern}");
-            println!("found prefix:{pattern} for {design}");
-            if is_possible(design, patterns) {
-                return true;
-            }
-        }
-    }
-    println!("nope");
-    false
-}
-*/
