4beda742305d4174b588f5d729cfbadaf085568f
Zach Thompson <zach@allotropic.com>
2024-12-21 14:59:32 -0500

    Clean up day 19

--- day19/main.rs
+++ day19/main.rs
@@ -17,13 +17,9 @@ fn main() {
     let part2: usize = designs
         .iter()
         .map(|design| {
-            println!("_________________\ntrying {design}");
             let matches = matches(design, &patterns);
-            //println!("matches: {matches:?}");
             let mut visited = HashMap::new();
-            let solution = solutions(design.len(), &matches, 0, &mut visited);
-            //println!("got {solution}");
-            solution
+            solutions(design.len(), &matches, 0, &mut visited)
         })
         .sum();
 
@@ -36,83 +32,33 @@ fn solutions(
     level: usize,
     visited: &mut HashMap<usize, usize>,
 ) -> usize {
-    let spaces = format!("{:1$}", " ", level * 2);
-    //println!("{spaces}______level:{level}");
     if level > matches.len() {
         return 0;
     }
-
     if visited.contains_key(&level) {
         return *visited.get(&level).unwrap();
     }
-
     // Start with longest substring matches.
     let mut keys = matches[level].keys().collect::<Vec<_>>();
     keys.sort_by(|a, b| b.cmp(a));
 
     let mut result = 0;
     for len in keys {
-        //println!("{spaces}len:{len}");
         let match_count = matches[level].get(len).unwrap().len();
-        //println!("{spaces}match_count:{match_count}");
 
         #[allow(clippy::comparison_chain)]
         if *len == remaining {
-            //println!("{spaces}MATCH REMAINING: add {match_count}");
             result += match_count;
         } else if *len < remaining {
             let to_add = match_count * solutions(remaining - *len, matches, level + *len, visited);
-            //println!("{spaces}NOT MATCH REMAINING: add {to_add}");
             result += to_add;
         }
     }
+
     visited.insert(level, result);
-
-    //println!("{spaces}**return** result:{result}");
-    result
-}
-
-/*
-fn solutions(
-    design_len: usize,
-    matches: &[HashMap<usize, Vec<&str>>],
-    level: usize,
-    //visited: &mut Vec<usize>,
-    visited: &mut HashMap<usize, usize>,
-) -> usize {
-    println!("\nlevel:{level}");
-    if level > matches.len() {
-        return 0;
-    }
-    if visited.contains_key(&level) {
-        return *visited.get(&level).unwrap();
-    }
-
-    let mut keys = matches[level].keys().collect::<Vec<_>>();
-    keys.sort_by(|a, b| b.cmp(a));
-
-    let mut result = 0;
-    for len in keys {
-        println!("  try len:{len}");
-        let match_count = matches.get(*len).unwrap().len();
-
-        if design_len == *len {
-            result += match_count;
-            println!("     end match, adding: {match_count}");
-        }
-        if design_len > *len {
-            let remaining = solutions(design_len - len, matches, level + len, visited);
-            result += match_count * remaining;
-            println!("     remaingin match, adding: {} * {} ", match_count, remaining );
-        }
-    }
-    visited.insert(level, result);
-
-    //println!("returning: {result}");
 
     result
 }
-*/
 
 fn is_possible(design: &str, patterns: &[&str]) -> bool {
     let matches = matches(design, patterns);
