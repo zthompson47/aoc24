5d121b5aeba224a093ebcb768aa9c9f74e55601a
Zach Thompson <zach@allotropic.com>
2024-12-21 20:28:05 -0500

    Solve day 20 part 2

--- day20/main.rs
+++ day20/main.rs
@@ -27,30 +27,41 @@ fn main() {
         })
         .collect();
     let (start, end) = (Position::from(start.unwrap()), Position::from(end.unwrap()));
-    let steps = count_steps(start, end, &track);
-    //println!("{}", steps.len());
-
-    let cheats = find_cheats(&steps, &track);
-    //println!("{:#?}", cheats.keys());
-
-    let part1 = cheats.iter().fold(0, |mut acc, (key, val)| {
-        if *key >= 100 {
-            acc += val.len();
-        }
-        acc
-    });
-
-    println!("Part 1: {part1}");
+    let steps = find_steps(start, end, &track);
+    let small_cheats = find_cheats(&steps, &track, CheatMode::Small);
+    let big_cheats = find_cheats(&steps, &track, CheatMode::Big);
+    fn count_cheats(cheats: &HashMap<usize, Vec<Cheat>>) -> usize {
+        cheats.iter().fold(0, |mut acc, (key, val)| {
+            if *key >= 100 {
+                acc += val.len();
+            }
+            acc
+        })
+    }
+    println!("Part 1: {}", count_cheats(&small_cheats));
+    println!("Part 2: {}", count_cheats(&big_cheats));
 }
 
-fn find_cheats(steps: &HashMap<Position, usize>, track: &Track) -> HashMap<usize, Vec<Cheat>> {
+enum CheatMode {
+    Small,
+    Big,
+}
+
+fn find_cheats(
+    steps: &HashMap<Position, usize>,
+    track: &Track,
+    mode: CheatMode,
+) -> HashMap<usize, Vec<Cheat>> {
     let mut result = HashMap::new();
     for (position, count) in steps {
-        for target in neighboring_cheats(*position, track) {
+        for (target, distance) in match mode {
+            CheatMode::Small => neighboring_cheats(*position, track),
+            CheatMode::Big => neighboring_big_cheats(*position, track),
+        } {
             if steps.contains_key(&target) {
                 let new_count = steps.get(&target).unwrap();
                 if new_count > count {
-                    let saved = new_count - count - 2;
+                    let saved = new_count - count - distance;
                     let cheat = Cheat {
                         start: *position,
                         end: target,
@@ -67,42 +78,96 @@ fn find_cheats(steps: &HashMap<Position, usize>, track: &Track) -> HashMap<usize
     result
 }
 
-fn neighboring_cheats(Position { row, column }: Position, track: &Track) -> Vec<Position> {
-    let mut result = Vec::new();
+fn neighboring_big_cheats(
+    Position { row, column }: Position,
+    track: &Track,
+) -> HashMap<Position, usize> {
+    let mut result = HashMap::new();
+    let (row, column) = (row as isize, column as isize);
+    for i in -20..=20 {
+        let mid_column = column + i;
+        if mid_column > 0 && mid_column < track[0].len() as isize - 1 {
+            if track[row as usize][mid_column as usize] == Cell::Space {
+                result.insert(
+                    Position {
+                        row: row as usize,
+                        column: mid_column as usize,
+                    },
+                    i.unsigned_abs(),
+                );
+            }
+            for j in -(20 - i.abs())..=20 - i.abs() {
+                let mid_row = row + j;
+                #[allow(clippy::collapsible_if)]
+                if mid_row > 0 && mid_row < track.len() as isize - 1 {
+                    if track[mid_row as usize][mid_column as usize] == Cell::Space {
+                        result.insert(
+                            Position {
+                                row: mid_row as usize,
+                                column: mid_column as usize,
+                            },
+                            i.unsigned_abs() + j.unsigned_abs(),
+                        );
+                    }
+                }
+            }
+        }
+    }
+    result
+}
+
+fn neighboring_cheats(
+    Position { row, column }: Position,
+    track: &Track,
+) -> HashMap<Position, usize> {
+    let mut result = HashMap::new();
     if row > 2 && track[row - 1][column] == Cell::Wall && track[row - 2][column] == Cell::Space {
-        result.push(Position {
-            row: row - 2,
-            column,
-        });
+        result.insert(
+            Position {
+                row: row - 2,
+                column,
+            },
+            2,
+        );
     }
     if row < track.len() - 2
         && track[row + 1][column] == Cell::Wall
         && track[row + 2][column] == Cell::Space
     {
-        result.push(Position {
-            row: row + 2,
-            column,
-        });
+        result.insert(
+            Position {
+                row: row + 2,
+                column,
+            },
+            2,
+        );
     }
 
     if column > 2 && track[row][column - 1] == Cell::Wall && track[row][column - 2] == Cell::Space {
-        result.push(Position {
-            row,
-            column: column - 2,
-        });
+        result.insert(
+            Position {
+                row,
+                column: column - 2,
+            },
+            2,
+        );
     }
     if column < track.len() - 2
         && track[row][column + 1] == Cell::Wall
         && track[row][column + 2] == Cell::Space
     {
-        result.push(Position {
-            row,
-            column: column + 2,
-        });
+        result.insert(
+            Position {
+                row,
+                column: column + 2,
+            },
+            2,
+        );
     }
     result
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 struct Cheat {
     start: Position,
@@ -110,7 +175,7 @@ struct Cheat {
     saved: usize,
 }
 
-fn count_steps(start: Position, end: Position, track: &Track) -> HashMap<Position, usize> {
+fn find_steps(start: Position, end: Position, track: &Track) -> HashMap<Position, usize> {
     let mut result = HashMap::new();
     result.insert(start, 0);
     let mut space = start;
@@ -174,40 +239,3 @@ impl From<(usize, usize)> for Position {
         Position { row, column }
     }
 }
-
-/*
-#[derive(Debug, PartialEq)]
-enum Direction {
-    N,
-    E,
-    S,
-    W,
-}
-
-impl Direction {
-    fn opposite(&self) -> Self {
-        match self {
-            Direction::N => Direction::S,
-            Direction::E => Direction::W,
-            Direction::S => Direction::N,
-            Direction::W => Direction::E,
-        }
-    }
-}
-
-impl From<(Position, Position)> for Direction {
-    fn from((start, end): (Position, Position)) -> Self {
-        if start.row < end.row {
-            Direction::S
-        } else if start.column < end.column {
-            Direction::E
-        } else if start.row > end.row {
-            Direction::N
-        } else if start.column > end.column {
-            Direction::W
-        } else {
-            unreachable!()
-        }
-    }
-}
-*/
